70132102e22488759d7115f9c2840b2afe598ebd
-Scriptix- <andygeng2023@gmail.com>
2026-09-25 19:01:36 +0100

    Document private ingestion token

--- web/config/database.example.php
+++ web/config/database.example.php
@@ -1,2 +1,9 @@
 <?php
-return ['host'=>'sqlXXX.infinityfree.com','name'=>'YOUR_DATABASE','user'=>'YOUR_USERNAME','pass'=>'YOUR_PASSWORD','charset'=>'utf8mb4'];
+return [
+  'host'=>'sqlXXX.infinityfree.com',
+  'name'=>'YOUR_DATABASE',
+  'user'=>'YOUR_USERNAME',
+  'pass'=>'YOUR_PASSWORD',
+  'charset'=>'utf8mb4',
+  'ingest_token'=>'GENERATE_A_LONG_RANDOM_TOKEN'
+];
